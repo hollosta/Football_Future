103e0f9a55eacff862a9c1d1a20e6c13a7ea8ca0
hollosta <tamas.hollos12@gmail.com>
2021-02-10 12:56:35 +0000

    Random data calculated

--- Script.js
+++ Script.js
@@ -1,1 +1,10 @@
-console.log("Football future generator");
+console.log("Football Future generator");
+const players = ['Kylian Mbappé', 'Christiano Ronaldo', 'Dominik Hollos', 'Luis Suarez', 'Peter Gulacsi', 'Dominik Szoboszlai', 'Willie Orban', 'Robert Lewandowsky', 'Zlatan Ibrahimovic', 'Eden Hazard', 'Mohamed Salah', 'Romelo Lukaku'];
+const teams = ['PSG', 'RB Leipzig', 'Liverpool', 'Ferencvaros', 'Manchester United', 'Bayern Munchen', 'Barcelona', 'Juventus', 'Inter Milan', 'Dortmund', 'Real Madrid', 'Ajax'];
+const year = [2028, 2029, 2030, 2031, 2032, 2033, 2034, 2035, 2036, 2037, 2038, 2039];
+
+const randomPlayer = players[Math.floor(Math.random() * 12)];
+
+const randomTeam = teams[Math.floor(Math.random() * 12)];
+
+const randomYear = year[Math.floor(Math.random() * 12)];
